a99ae68ff84c90a32f9821ebe5d7039ab5e153ec
Luiz Gustavo <lgustavsb@gmail.com>
2022-09-10 12:07:22 -0300

    exercicios js
    Exercício fluxo de caixa familiar e exercicio de conversão de celsius para fahrenheit js

--- fluxoDeCaixaFamiliar.js
+++ fluxoDeCaixaFamiliar.js
@@ -8,3 +8,37 @@
 * despesas e irá mostrar uma mensagem se a família está com 
 * saldo positivo ou negativo, seguido do valor do saldo. 
 */
+
+let receitas = [];
+let despesas = [];
+
+let contas = {
+    receitas: [2500, 3200, 250.43, 360.45],
+    despesas: [320.34, 128.45, 176.87, 1450.00]
+}
+    
+function somar(array) {
+    let total = 0;
+
+    for(let value of array){
+        total += value
+    }
+    return total;
+}
+
+function calcularContas (){
+    const calcularReceitas = somar(contas.receitas);
+    const calcularDespesas = somar(contas.despesas);
+
+    const total = calcularReceitas - calcularDespesas;
+
+    const saldoPositivo = total >= 0;
+
+    if(saldoPositivo) {
+        console.log("Saldo Positivo: R$" + total.toFixed(2)  + " :)")
+    }else{
+        console.log("Saldo negativo: R$" + total.toFixed(2) + " :(");
+    }
+}
+
+calcularContas();
